91c3b608934c95c9eddcf842998cbe551fbb68ac
JakubRad <kubaradziwoniuk@gmail.com>
2022-10-21 23:44:44 +0200

    defence attribute implemented

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from models import Character
 
-p1 = Character(name="John", hp=100, strength=15)
-p2 = Character("Mike", hp=100, strength=10)
+p1 = Character(name="John", hp=100, strength=15, defence=5)
+p2 = Character("Mike", hp=100, strength=10, defence=3)
 
 p1.attack(p2)
 print(p2.hp)
