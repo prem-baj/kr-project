0618a7a144a8df6fc19967cf66a46ca0910ba791
ratkoski <ratkoski@wp.pl>
2022-10-24 20:08:45 +0200

    fighting functionality not working

--- models.py
+++ models.py
@@ -1,15 +1,21 @@
+from typing import DefaultDict
+
+
 class Character:
     def __init__(self, name, hp=100.0, strength=0.0, defence=1.0):
         self.name = name
         self.hp = hp
         self.strength = strength
         self.defence = defence
-
-    def attack(self, opponent):
+        
+    def attack(self,opponent): 
         damage = self.strength - opponent.defence
-        if damage < 1:
-            damage = 1
-        opponent.hp = opponent.hp - damage
-        print(f"{self.name} attacks {opponent.name} and deals {damage} damage")
-
-
+        while opponent.hp >= 0:
+            if opponent.hp <= 100:
+                opponent.hp -= damage
+                print ('%s hits %s for %s, %s hp left %s' %(self.name,opponent.name,damage,opponent.name,opponent.hp))
+            elif opponent.hp <= 0:
+                print ('%s has killed %s' %(self.name,opponent.name))
+            if self.hp <= 0:
+                print ("%s is dead" %(self.name))
+                   
